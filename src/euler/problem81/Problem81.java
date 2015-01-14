c62985b156a79b98d46f3e42371490e439bc11d2
Mihails Volkovs <mihails.volkovs@gmail.com>
2015-01-14 23:35:19 +0200

    Solution for small data set

--- src/euler/problem81/Problem81.java
+++ src/euler/problem81/Problem81.java
@@ -5,23 +5,114 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by Mihails Volkovs on 2015.01.12.
  */
 public class Problem81 {
 
+    private static long completePaths = 0;
+
+    private static long minimumTotal = Long.MAX_VALUE;
+
+    private static PathNode result = null;
+
     public static final void main(String... args) throws IOException, URISyntaxException {
-//        IntegerMatrix matrix = new IntegerMatrix("https://projecteuler.net/project/resources/p081_matrix.txt");
-        IntegerMatrix matrix = new IntegerMatrix(
-                "131, 673, 234, 103, 18" + "\n" +
-                        "201, 96, 342, 965, 150" + "\n" +
-                        "630, 803, 746, 422, 111" + "\n" +
-                        "537, 699, 497, 121, 956" + "\n" +
-                        "805, 732, 524, 37, 331");
-
-
+        IntegerMatrix matrix = new IntegerMatrix("https://projecteuler.net/project/resources/p081_matrix.txt");
+//        IntegerMatrix matrix = new IntegerMatrix(
+//                "131, 673, 234, 103, 18" + "\n" +
+//                        "201, 96, 342, 965, 150" + "\n" +
+//                        "630, 803, 746, 422, 111" + "\n" +
+//                        "537, 699, 497, 121, 956" + "\n" +
+//                        "805, 732, 524, 37, 331");
         matrix.out();
+
+
+        generateNext(new PathNode(matrix, 0, 0, null));
+
+        System.out.println("Generated paths: " + completePaths);
+        System.out.println("The shortest path is: " + result.getTotal());
+        System.out.println("The shortest path is: " + result.toString());
+    }
+
+    private static void generateNext(PathNode node) {
+        PathNode nextNode1 = node.getNextHorizontally();
+        if (nextNode1 != null) {
+            generateNext(nextNode1);
+        }
+
+        PathNode nextNode2 = node.getNextVertically();
+        if (nextNode2 != null) {
+            generateNext(nextNode2);
+        }
+
+        // path completed - adding to the list
+        if (nextNode1 == null && nextNode2 == null) {
+            completePaths++;
+            if (node.getTotal() < minimumTotal) {
+                minimumTotal = node.getTotal();
+                result = node;
+            }
+            if (completePaths % 10000000 == 0) {
+                System.out.println("Completed paths: " + completePaths);
+                System.out.println("Recent path is: " + node.toString());
+                System.out.println("Current result is: " + result.toString());
+                System.out.println("Current result is: " + minimumTotal);
+            }
+        }
+    }
+
+    public static class PathNode implements Comparable<PathNode> {
+        private IntegerMatrix matrix;
+        private int row;
+        private int column;
+        private PathNode previous;
+        private long total;
+        private int value;
+
+        public PathNode(IntegerMatrix matrix, int row, int column, PathNode previous) {
+            this.matrix = matrix;
+            this.row = row;
+            this.column = column;
+            this.previous = previous;
+            long previousTotal = previous == null ? 0 : previous.getTotal();
+            this.value = matrix.get(row, column);
+            this.total = previousTotal + value;
+        }
+
+        public PathNode getNextHorizontally() {
+            int nextColumn = column + 1;
+            if (nextColumn >= matrix.columnsCount()) {
+                return null;
+            }
+            return new PathNode(matrix, row, nextColumn, this);
+        }
+
+        public PathNode getNextVertically() {
+            int nextRow = row + 1;
+            if (nextRow >= matrix.rowsCount()) {
+                return null;
+            }
+            return new PathNode(matrix, nextRow, column, this);
+        }
+
+        public long getTotal() {
+            return total;
+        }
+
+        @Override
+        public int compareTo(PathNode pathNode) {
+            return Long.compare(total, pathNode.total);
+        }
+
+        @Override
+        public String toString() {
+            return (previous == null ? "" : previous.toString()) + String.format("[%s, %s] (%s), ", row, column, value);
+        }
     }
 
     public static class IntegerMatrix {
@@ -48,6 +139,10 @@ public class Problem81 {
             }
         }
 
+        public int get(int row, int column) {
+            return cells[row][column];
+        }
+
         public void out() {
             System.out.println("Matrix: ");
             for (int[] row : cells) {
@@ -56,6 +151,14 @@ public class Problem81 {
                 }
                 System.out.println();
             }
+        }
+
+        public int rowsCount() {
+            return cells.length;
+        }
+
+        public int columnsCount() {
+            return cells.length == 0 ? 0 : cells[0].length;
         }
     }
 
