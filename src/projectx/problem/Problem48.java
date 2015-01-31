68dd801cb2e327afd1363d3d66cf7aeab5a14cdd
Mihails Volkovs <mihails.volkovs@gmail.com>
2015-02-01 00:07:24 +0200

    New problem solved

--- src/projectx/problem/Problem48.java
+++ src/projectx/problem/Problem48.java
@@ -7,26 +7,35 @@ import com.google.common.math.BigIntegerMath;
  */
 public class Problem48 {
 
-    private static final int LIMIT = 10;
+    private static final int LIMIT = 1000;
 
-    private static final long RESULT_LIMIT = 10;
+    private static final int RESULT_LIMIT = 10;
 
     public static final void main(String... args) {
-//        System.out.println(Math.pow(1000, 1000));
-
         long result = 0;
         for (int i = 1; i <= LIMIT; i++) {
-            result += Math.pow(i, i);
-            while (result > Math.pow(10, RESULT_LIMIT)) {
-//                System.out.println(result + " " + result / 10);
-                result /= 10;
-            }
+            result += power(i, i);
+            result = trim(result);
         }
         System.out.println(result);
     }
 
-    private static class MyNumber {
+    private static long power(int principal, int power) {
+        long result = principal;
+        for (int i = 1; i < power; i++) {
+            System.out.println(" - " + result);
+            result *= principal;
+            result = trim(result);
+        }
+        return result;
+    }
 
+    private static long trim(long tooLongNumber) {
+        String number = "" + tooLongNumber;
+        if (number.length() <= RESULT_LIMIT) {
+            return tooLongNumber;
+        }
+        return Long.parseLong(number.substring(number.length() - RESULT_LIMIT - 1));
     }
 
 }
